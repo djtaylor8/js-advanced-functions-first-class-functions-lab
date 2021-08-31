523a60c9dd8e4615c93413397fba228584ec072a
DJ Taylor <dtaylor88@gmail.com>
2021-08-30 19:28:39 -0700

    commit tree 436f57977d0a655fc3c66c227e5bf0598dc0e03b

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(array) {
+    const [a, b] = array;
+    return [a, b];
+}
+
+const returnLastTwoDrivers = function(array) {
+    const [a, b] = array.slice(-2);
+    return [a, b];
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+function createFareMultiplier(mult) {
+    return function(fare) {
+        return fare * mult
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(array, a = returnFirstTwoDrivers, b = returnLastTwoDrivers) {
+    if (a === undefined) {
+        return b(array);
+    } else {
+        return a(array);
+    }
+}
+
